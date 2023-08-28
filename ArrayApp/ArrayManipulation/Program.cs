8ec6f696a50de3c6203688b082aff472bccd214c
AverySims <avery.j.sims@gmail.com>
2023-08-28 11:00:00 -0500

    clean up and loop management

--- ArrayApp/ArrayManipulation/Program.cs
+++ ArrayApp/ArrayManipulation/Program.cs
@@ -1,153 +1,214 @@
-﻿namespace ArrayManipulation
+﻿using ConsoleFunctions;
+using GenericParse;
+
+
+namespace ArrayManipulation
 {
 	internal class Program
 	{
-		static int[] array;
-		static int size;
+		static double[] customArray;
+		static int customSize;
+		
+		private static readonly string[] MenuOptions = 
+			{"Find minimum and maximum", 
+				"Calculate sum and average", 
+				"Reverse the array", 
+				"Sort the array", 
+				"Add element", 
+				"Remove element", 
+				"View elements",
+				"Exit program"
+			};
 
 		static void Main(string[] args)
 		{
-			Console.Write("Enter the initial size of the array: ");
-			size = int.Parse(Console.ReadLine());
+			bool loopMain = true;
+			bool loopSelections = true;
 
-			array = new int[size];
-
-			Console.WriteLine("Enter the elements of the array:");
-			for (int i = 0; i < size; i++)
+			while (loopMain)
 			{
-				Console.Write($"Element {i + 1}: ");
-				array[i] = int.Parse(Console.ReadLine());
+				InitializeArray();
+				
+				while (loopSelections)
+				{
+					PrintMenu();
+					SelectMenuOption();
+				}
+				
+			}
+		}
+
+		static void InitializeArray()
+		{
+			bool tempValid = false;
+			
+			// user input for array size
+			Console.Write("Enter the size of your array: ");
+			do
+			{
+				customSize = GenericReadLine.TryReadLine<int>();
+				if (customSize >= 1)
+				{
+					// valid array size, continue program
+					tempValid = true;
+				}
+				else
+				{
+					// array size invalid
+					ConsoleHelper.PrintInvalidSelection();
+				}
+			} while( !tempValid );
+
+			// initializing new array
+			customArray = new double[customSize];
+
+			Console.WriteLine("Enter the elements of your array:");
+			for (int i = 0; i < customSize; i++)
+			{
+				Console.Write($"Index {i}: ");
+				customArray[i] = GenericReadLine.TryReadLine<double>();
+			}
+		}
+		
+		static void PrintMenu()
+		{
+			Console.WriteLine("- - - Menu - - -");
+
+			for (int i = 0; i < MenuOptions.Length; i++)
+			{
+				Console.WriteLine($"{i + 1}. {MenuOptions[i]}");
 			}
 
-			bool exit = false;
-			while (!exit)
-			{
-				PrintMenu();
-				int choice = int.Parse(Console.ReadLine());
+			Console.Write("Enter your choice: ");
+		}
 
-				switch (choice)
+		static void SelectMenuOption()
+		{
+			bool loopTemp = true;
+
+			int tempSelect = 0;
+
+			while (loopTemp)
+			{
+				bool tempValid = false;
+				Console.Write("Select option: ");
+				do
+				{
+					 tempSelect = GenericReadLine.TryReadLine<int>();
+					if (tempSelect >= 1 && tempSelect <= MenuOptions.Length)
+					{
+						// selection valid, continue program
+						tempValid = true;
+					}
+					else
+					{
+						// selection invalid
+						ConsoleHelper.PrintInvalidSelection();
+					}
+				} while (!tempValid);
+
+				switch (tempSelect)
 				{
 					case 1:
 						FindMinMax();
 						break;
-
 					case 2:
 						CalculateSumAndAverage();
 						break;
-
 					case 3:
 						ReverseArray();
 						break;
-
 					case 4:
 						SortArray();
 						break;
-
 					case 5:
 						AddElement();
 						break;
-
 					case 6:
 						RemoveElement();
 						break;
-
 					case 7:
 						ViewArray();
 						break;
-
 					case 8:
-						exit = true;
+						// exit = true;
 						Console.WriteLine("Exiting program.");
 						break;
-
 					default:
-						Console.WriteLine("Invalid choice. Please enter a valid option.");
+						ConsoleHelper.PrintInvalidSelection();
 						break;
 				}
+				
 			}
+			
 		}
-
-		static void PrintMenu()
-		{
-			Console.WriteLine("\nMenu:");
-			Console.WriteLine("1. Find minimum and maximum");
-			Console.WriteLine("2. Calculate sum and average");
-			Console.WriteLine("3. Reverse the array");
-			Console.WriteLine("4. Sort the array");
-			Console.WriteLine("5. Add element");
-			Console.WriteLine("6. Remove element");
-			Console.WriteLine("7. View array");
-			Console.WriteLine("8. Exit");
-			Console.Write("Enter your choice: ");
-		}
-
+		
 		static void FindMinMax()
 		{
-			int min = array[0];
-			int max = array[0];
-			for (int i = 1; i < size; i++)
+			var min = customArray[0];
+			var max = customArray[0];
+			for (int i = 1; i < customSize; i++)
 			{
-				if (array[i] < min)
-					min = array[i];
-				if (array[i] > max)
-					max = array[i];
+				if (customArray[i] < min)
+					min = customArray[i];
+				if (customArray[i] > max)
+					max = customArray[i];
 			}
-			Console.WriteLine($"Minimum: {min}");
-			Console.WriteLine($"Maximum: {max}");
+			Console.WriteLine($"Minimum: {min}, Maximum: {max}");
 		}
 
 		static void CalculateSumAndAverage()
 		{
-			int sum = 0;
-			for (int i = 0; i < size; i++)
+			double sum = 0;
+			for (int i = 0; i < customSize; i++)
 			{
-				sum += array[i];
+				sum += customArray[i];
 			}
-			double average = (double)sum / size;
+			double average = sum / customSize;
 			Console.WriteLine($"Sum: {sum}");
 			Console.WriteLine($"Average: {average}");
 		}
 
 		static void ReverseArray()
 		{
-			Array.Reverse(array);
+			Array.Reverse(customArray);
 			Console.WriteLine("Array reversed.");
 		}
 
 		static void SortArray()
 		{
-			Array.Sort(array);
+			Array.Sort(customArray);
 			Console.WriteLine("Array sorted.");
 		}
 
 		static void AddElement()
 		{
 			Console.Write("Enter the element to add: ");
-			int newElement = int.Parse(Console.ReadLine());
+			var newElement = GenericReadLine.TryReadLine<double>();
 
-			if (size == array.Length)
+			if (customSize == customArray.Length)
 			{
-				Array.Resize(ref array, size * 2); // Double the array size
+				Array.Resize(ref customArray, customSize * 2); // Double the array size
 			}
 
-			array[size] = newElement;
-			size++;
+			customArray[customSize] = newElement;
+			customSize++;
 			Console.WriteLine("Element added.");
 		}
 
 		static void RemoveElement()
 		{
-			if (size > 0)
+			if (customSize > 0)
 			{
 				Console.Write("Enter the index of the element to remove: ");
-				int indexToRemove = int.Parse(Console.ReadLine());
-				if (indexToRemove >= 0 && indexToRemove < size)
+				int indexToRemove = GenericReadLine.TryReadLine<int>();
+				if (indexToRemove >= 0 && indexToRemove < customSize)
 				{
-					for (int i = indexToRemove; i < size - 1; i++)
+					for (int i = indexToRemove; i < customSize - 1; i++)
 					{
-						array[i] = array[i + 1];
+						customArray[i] = customArray[i + 1];
 					}
-					size--;
+					customSize--;
 					Console.WriteLine("Element removed.");
 				}
 				else
@@ -164,9 +225,9 @@
 		static void ViewArray()
 		{
 			Console.WriteLine("Array Contents:");
-			for (int i = 0; i < size; i++)
+			for (int i = 0; i < customSize; i++)
 			{
-				Console.WriteLine($"Index {i}: {array[i]}");
+				Console.WriteLine($"Index {i}: {customArray[i]}");
 			}
 		}
 	}
